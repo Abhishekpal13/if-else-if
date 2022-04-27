86f7d3a9d2934c7d23fdc2503d29e389a6c698a5
Abhishek pal <102280420+Abhishekpal13@users.noreply.github.com>
2022-04-28 03:45:06 +0530

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,10 @@
+let score = 6;
+if (score > 8) {
+    console.log("A");
+} else if (score >=6 && score < 8) {
+    console.log("B");
+} else if (score >= 4 && score < 6) {
+    console.log("C");
+} else {
+    console.log("D");
+} 
